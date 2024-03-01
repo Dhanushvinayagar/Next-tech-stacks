fa61a5de1eeb2ce0464622c69559ae95ecdcbc6a
Dhanushvinayagar <dhanush22092002@gmail.com>
2024-03-01 21:49:15 +0530

    Added catch all route segments

---
+++ src/app/catchallpath/[...slug]/page.jsx
@@ -0,0 +1,47 @@
+export default function Slug({ params }) {
+    // render this for 
+    if (params.slug.length === 1)
+        return (
+            <>
+                <p>
+                    Enter any path with n number of routes
+                </p>
+                <p>
+                    Try n number of route to see magic
+                </p>
+                <p>
+                    Eg:
+                    1- one
+                    2- one/two
+                    3- one/two/three
+                </p>
+            </>
+        )
+
+    if (params.slug.length === 2)
+        return (
+            <>
+                <p>
+                    The path is stored in slug as array element
+                </p>
+                <p>
+                    Can render any component here based on condition
+                </p>
+            </>
+        )
+
+    return <>
+        {
+            params.slug.map((el, i) => {
+                return (
+                    <>
+                        <p>Try adding more</p>
+                        <div key={i}>
+                            <p>{el}</p>
+                        </div>
+                    </>
+                )
+            })
+        }
+    </>
+}
---
+++ src/app/catchallpath/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Catchallroute() {
+    return (
+      <>
+        <h1>
+          The slug with [...foldername] helps to catch all path ([...slug] more conventional)
+        </h1>
+      </>
+    );
+  }
+  
